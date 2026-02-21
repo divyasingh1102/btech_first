7da18c054c80d1d402d1f7b0f10eb500269f5bbf
divya <[divyasingh25044@gmail.com>
2026-02-21 10:51:02 +0530

    p added

---
+++ app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const app = () => {
+    const [color, setcolor] = usestste("red");
+    return (
+        <div>
+            <button>green</button>
+            <button>pink</button>
+            <button>blue</button>
+            <button>yellow</button>
+        </div>
+    )
+}
+
+export default app
